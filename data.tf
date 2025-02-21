af9ff34a46db9358cef383bd0dc2a15c810b5789
SAISUMANTHGOTTIPATI <108699723+SAISUMANTHGOTTIPATI@users.noreply.github.com>
2025-02-21 07:30:27 +0000

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,1 +1,16 @@
 
+data "azurerm_virtual_network" "vnet" {
+  name                = local.vnet_name
+  resource_group_name = local.vnet_rg
+}
+
+data "azurerm_subnet" "aks_subnet" {
+  name                 = var.aks_subnet_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_virtual_network.vnet.resource_group_name
+}
+
+data "azurerm_route_table" "rt_vnet" {
+  name                = local.route_table_name
+  resource_group_name = data.azurerm_virtual_network.vnet.resource_group_name
+}
